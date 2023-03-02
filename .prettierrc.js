252107e8c26488f5b93ab032633f1a091c5a189c
DESKTOP-1V9OQ1L\GIGABYTE <emma05127@gmail.com>
2023-03-02 15:30:53 +0900

    🎉 start init
    프로젝트 파일 생성 및 불필요 파일 삭제

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+	semi: true,
+	singleQuote: true, // 문자열을 사용 할 때에는 " 를 사용
+	useTabs: true, // tab 대신에 스페이스를 사용
+	tabWidth: 2, // 들여쓰기 크기
+	trailingComma: 'all', // 객체나 배열의 마지막 값에도 , 를 붙임
+	printWidth: 100, // printWidth default 80 => 100 으로 변경
+	singleQuote: true, // "" => ''
+	arrowParens: 'avoid', // arrow function parameter가 하나일 경우 괄호 생략
+};
