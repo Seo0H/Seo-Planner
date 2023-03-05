44ddff18b236818b15c9b886819cd25b51ff89dc
DESKTOP-1V9OQ1L\GIGABYTE <emma05127@gmail.com>
2023-03-05 21:34:44 +0900

    ✨ line-change 구현 완료
    > 2열로 보여야 하는 BlockContainor 를 구현하기 위해 컴포넌트 분리 진행
    > 스크롤이 되면 안되기 때문에 일단 임의로 22라인까지를 한 열의 최대 라인 수로 설정
    > 컴포넌트 분리로 인한BlocksContainerProps와 TextMemoForms interface 명시적 추가

--- src/components/main-field/BlocksContainer.tsx
+++ src/components/main-field/BlocksContainer.tsx
@@ -1,7 +1,30 @@
-import React from 'react';
+import * as S from './styles';
+import Block from './Block';
+import { BlocksContainerProps, BlockTypes } from './types';
 
-function BlocksContainer(props) {
-	return <div></div>;
+function BlocksContainer({ planActive, doActive, textMemoInput, onClick }: BlocksContainerProps) {
+	return (
+		<S.BlockContainer>
+			<S.TimeContainor style={{ gridArea: 'time' }}>
+				{planActive.map(el => (Number.isInteger(el.time) ? <div>{el.time}</div> : <div></div>))}
+			</S.TimeContainor>
+			<div className={BlockTypes.PLAN}>
+				{planActive.map(el => (
+					<Block form={el} key={el.key} id={el.key.toString()} onClick={onClick} />
+				))}
+			</div>
+			<div className={BlockTypes.DONE}>
+				{doActive.map(el => (
+					<Block form={el} key={el.key} id={el.key.toString()} onClick={onClick} />
+				))}
+			</div>
+			<div>
+				{textMemoInput.map(el => (
+					<S.TextMemoInput key={el.key} />
+				))}
+			</div>
+		</S.BlockContainer>
+	);
 }
 
 export default BlocksContainer;
--- src/components/main-field/types.ts
+++ src/components/main-field/types.ts
@@ -3,9 +3,21 @@ export interface BlockForms {
 	active: boolean;
 	time: number;
 }
+
+export interface TextMemoForms {
+	key: number;
+	text: string;
+}
 export interface BlockProps {
 	id: string;
 	form: BlockForms;
+	onClick: (e: React.MouseEvent) => void;
+}
+
+export interface BlocksContainerProps {
+	planActive: BlockForms[];
+	doActive: BlockForms[];
+	textMemoInput: TextMemoForms[];
 	onClick: (e: React.MouseEvent) => void;
 }
 
