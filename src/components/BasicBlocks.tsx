f1c613023a4ab4e96f96e63172fa7c38843c1f35
DESKTOP-1V9OQ1L\GIGABYTE <emma05127@gmail.com>
2023-03-05 18:48:36 +0900

    🚧 작동 요소 구성중

--- src/components/BasicBlocks.tsx
+++ src/components/BasicBlocks.tsx
@@ -1,57 +1,116 @@
 import React from 'react';
 import { useState, useEffect, useCallback } from 'react';
+import styled from 'styled-components';
 import Block from './Block';
+import { BlockTypes } from './types';
+import * as S from './styles';
 
 //한시간 -> 2block
 // 7:00 ~ 24:00 -> 17시간 -> 34 block 나와야 함.
 // 나중에 이것도 유저가 선택 할 수 있게 하면 좋을 것 같다.
-const initialBlockTemplet = [
+const initialPlanBlockTemplet = [
+	{
+		key: 0,
+		isActive: false,
+	},
+];
+const initialDoBlockTemplet = [
 	{
 		key: 0,
 		isActive: false,
 	},
 ];
 
-export type BlockProps = {
-	id: string;
-	key: number;
-	active: boolean;
-	onClick: (e: React.MouseEvent) => void;
-};
+//몇자 이상 넘어가면 다음줄로 넘어가게 처리해야...
+const initialTextMemoInput = [
+	{
+		key: 0,
+		text: '',
+	},
+];
 
 const BasicBlocks = () => {
-	const [active, setActive] = useState(initialBlockTemplet);
+	const [planActive, setPlanActive] = useState(initialPlanBlockTemplet);
+	const [doActive, setDoActive] = useState(initialDoBlockTemplet);
+	const [textMemoInput, setTextMemoInput] = useState(initialTextMemoInput);
 
+	// 처음 한번만 랜더링
 	useEffect(() => {
 		let pushVal = [];
-		for (let i = 1; i <= 34; i++) {
+		let pushMemoVal = [];
+		for (let i = 1; i <= 20; i++) {
 			pushVal.push({
 				key: i,
 				isActive: false,
 			});
+
+			pushMemoVal.push({
+				key: i,
+				text: '',
+			});
 		}
-		setActive(active.concat(pushVal));
+		setDoActive(doActive.concat(pushVal));
+		setPlanActive(planActive.concat(pushVal));
+		setTextMemoInput(textMemoInput.concat(pushMemoVal));
 	}, []);
 
 	const onClick = useCallback(
 		(e: React.MouseEvent) => {
 			e.preventDefault();
-			const thisId = parseInt(e.currentTarget.id);
-			const thisIdx = active.findIndex(i => i.key === thisId);
-			const copyArr = [...active];
-			copyArr[thisIdx] = { ...copyArr[thisIdx], isActive: !copyArr[thisIdx].isActive };
-			setActive(copyArr);
+
+			const element = e.target as Element;
+			const parentElement = element.parentElement;
+
+			const clickBoxID = parseInt(e.currentTarget.id);
+			if (parentElement?.className.includes(BlockTypes.PLAN)) {
+				const clickBoxIdx = planActive.findIndex(i => i.key === clickBoxID);
+				const copyArr = [...planActive];
+				copyArr[clickBoxIdx] = {
+					...copyArr[clickBoxIdx],
+					isActive: !copyArr[clickBoxIdx].isActive,
+				};
+				setPlanActive(copyArr);
+			} else if (parentElement?.className.includes(BlockTypes.DONE)) {
+				const clickBoxIdx = doActive.findIndex(i => i.key === clickBoxID);
+				const copyArr = [...doActive];
+				copyArr[clickBoxIdx] = {
+					...copyArr[clickBoxIdx],
+					isActive: !copyArr[clickBoxIdx].isActive,
+				};
+				setDoActive(copyArr);
+			}
 		},
-		[active],
+		[planActive, doActive],
 	);
 
 	return (
-		<>
-			<h1>basicBlocks</h1>
-			{active.map(el => (
-				<Block key={el.key} id={el.key.toString()} active={el.isActive} onClick={onClick} />
-			))}
-		</>
+		<S.MainContainer>
+			<S.BlockContainer>
+				<S.ListWrapper className={BlockTypes.PLAN} style={{ gridArea: 'block,' }}>
+					{planActive.map(el => (
+						<Block key={el.key} id={el.key.toString()} active={el.isActive} onClick={onClick} />
+					))}
+				</S.ListWrapper>
+				<S.ListWrapper>
+					{textMemoInput.map(el => (
+						<S.TextMemoInput key={el.key} style={{ gridArea: 'line' }} autoComplete="off" />
+					))}
+				</S.ListWrapper>
+			</S.BlockContainer>
+
+			<S.BlockContainer>
+				<S.ListWrapper className={BlockTypes.DONE} style={{ gridArea: 'block' }}>
+					{doActive.map(el => (
+						<Block key={el.key} id={el.key.toString()} active={el.isActive} onClick={onClick} />
+					))}
+				</S.ListWrapper>
+				<S.ListWrapper>
+					{textMemoInput.map(el => (
+						<S.TextMemoInput key={el.key} style={{ gridArea: 'line' }} autoComplete="off" />
+					))}
+				</S.ListWrapper>
+			</S.BlockContainer>
+		</S.MainContainer>
 	);
 };
 
