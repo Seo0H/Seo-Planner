9ab8a034ae09e12304908188d8831f0d6a92f839
DESKTOP-1V9OQ1L\GIGABYTE <emma05127@gmail.com>
2023-03-05 20:46:40 +0900

    🏷️ BlockForms Type 분리
    >BlockProps에 종속되어 있던 집중Block의 고유한 값(시간, 활성상태,  인덱스키값)의 타입을 인터페이스로 분리하였다.

--- src/components/Block.tsx
+++ src/components/Block.tsx
@@ -1,7 +1,7 @@
 import * as S from './styles';
 import { BlockProps } from './types';
 
-const Block = ({ id, active = false, onClick }: BlockProps) => {
+const Block = ({ id, form: { active = false, time }, onClick }: BlockProps) => {
 	return <S.StyledBasicBlockInput id={id} active={active} onClick={onClick} />;
 };
 
